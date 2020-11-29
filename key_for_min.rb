1aa5f57bb3bd6c7f80d958edf1282e9650194e07
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 19:30:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 100
   min_key = nil 
-  name_hash.each do |value, key|
+  name_hash.each do |key,value|
    if value < min_value
       min_value = value
       min_key = key  
